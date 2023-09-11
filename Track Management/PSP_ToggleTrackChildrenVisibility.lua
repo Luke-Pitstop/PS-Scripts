310b69a78c2766796e505ec6ef749168dac1038f
Luke Willis <luke@pitstopproductions.co.uk>
2023-09-11 17:09:32 +0100

    Fixed Toggle Track Children Vis

--- Track Management/PSP_ToggleTrackChildrenVisibility.lua
+++ Track Management/PSP_ToggleTrackChildrenVisibility.lua
@@ -1,9 +1,9 @@
 --[[
 @author Luke Willis
-@version 1.0
+@version 1.1
 @licence GPL v3
 @reaper 6.82
-@changelog release
+@changelog fixed issue where it was referencing incorrect index
 @about Tries to duplicate items down to a track that is not collapsed or hidden
 --]]
 
@@ -14,12 +14,12 @@ local PROJECT = 0
 
 -----------------------------------------------------------------------
 
-local function getNewState(selectedTrackIndex)
+local function getNewState(selectedTrackIndex) -- 1 = show. 0 hide
     local nextTrack = r.GetTrack(PROJECT, selectedTrackIndex + 1)
     if not nextTrack then return 0 end
     local currentState = r.GetMediaTrackInfo_Value(nextTrack, "B_SHOWINTCP")
     if currentState == 0 then return 1 end
-    if currentState == 1 then return 0 end
+    return 0
 end
 
 -----------------------------------------------------------------------
@@ -30,12 +30,9 @@ local function updateChildrenVisiblity(parent, newState)
     for i = parentIndex + 1, r.CountTracks(PROJECT) - 1 do
         local searchTrack = r.GetTrack(PROJECT, i)
         local searchTrackDepth = r.GetTrackDepth(searchTrack)
-        if parentDepth ~= searchTrackDepth then
-            r.SetMediaTrackInfo_Value(searchTrack, "B_SHOWINTCP", newState)
-            r.SetMediaTrackInfo_Value(searchTrack, "B_SHOWINMIXER", newState)
-        else
-            return
-        end
+        if parentDepth == searchTrackDepth then return end
+        r.SetMediaTrackInfo_Value(searchTrack, "B_SHOWINTCP", newState)
+        r.SetMediaTrackInfo_Value(searchTrack, "B_SHOWINMIXER", newState)
     end
 end
 
@@ -44,18 +41,15 @@ end
 local function tryToggleVisibility()
     for i = 0, r.CountSelectedTracks(PROJECT) - 1 do
         local track = r.GetSelectedTrack(PROJECT, i)
-        local parent = r.GetParentTrack(track)
-        if not parent then
-            local isFolder = r.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH") == 1
-            if isFolder then
-                updateChildrenVisiblity(track, getNewState(i))
-                r.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", 0)
-            end
+        local isFolder = r.GetMediaTrackInfo_Value(track, "I_FOLDERDEPTH") == 1
+        if isFolder then
+            local trackIndex = r.GetMediaTrackInfo_Value(track, "IP_TRACKNUMBER") - 1
+            updateChildrenVisiblity(track, getNewState(trackIndex))
+            r.SetMediaTrackInfo_Value(track, "I_FOLDERCOMPACT", 0)
         end
     end
     r.TrackList_AdjustWindows(false)
 end
 
 -----------------------------------------------------------------------
-
 tryToggleVisibility()
